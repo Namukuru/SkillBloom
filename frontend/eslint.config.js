a819211736e2b1bd91c8ee6e01264bc4d4cf3379
soroel <sorophinenoel@gmail.com>
2025-03-10 23:47:22 +0300

    messaging ui

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,8 +1,3 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-
 export default [
   { ignores: ['dist'] },
   {
@@ -23,7 +18,7 @@ export default [
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]|motion$' }],
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
