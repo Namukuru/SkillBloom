9426e6ebf6858e507809f24af0b062a6b69dbb4b
soroel <sorophinenoel@gmail.com>
2025-03-10 12:39:40 +0300

    configuring tailwind

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import { fileURLToPath } from "url";  // Import helper to get __dirname
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url)); // Define __dirname
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(),],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), 
