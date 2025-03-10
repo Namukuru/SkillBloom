e469b51cac822b7722accfbb09168c68f768be0e
Namukuru Lauryn <namukurulauryn@gmail.com>
2025-03-10 09:58:47 +0300

    Registering models in the admin site.

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Skill, SkillMatch
 
 # Register your models here.
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
+    ordering = ('name',)
+    
+admin.site.register(Skill, SkillAdmin)
+admin.site.register(SkillMatch)
