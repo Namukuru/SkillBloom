d18d01b088a4f47601cd9ba10ee1643b4a1e60b9
Namukuru Lauryn <namukurulauryn@gmail.com>
2025-03-10 18:16:41 +0300

    Getting the correct skill.

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -9,9 +9,14 @@ class SkillAdmin(admin.ModelAdmin):
     ordering = ('name',)
 
 class CustomUserAdmin(admin.ModelAdmin):
-    list_display = ("email", "full_name", "proficiency")  # Show basic user details
+    list_display = ("email", "full_name", "proficiency","get_skills")  # Show basic user details
     search_fields = ("email", "full_name")  # Enable searching by email & name
     ordering = ("email",)  # Order by email
+    
+    def get_skills(self, obj):
+        return ", ".join([skill.name for skill in obj.skills.all()])
+
+    get_skills.short_description = 'Skills'  # Optional: Rename column in admin panel
 
     
 admin.site.register(Skill, SkillAdmin)
